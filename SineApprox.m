36ea11ff20161fa60c646c55fd940a542aa98330
Tortu16 <luisdjm@gmail.com>
2016-03-13 12:13:04 -0600

    Complete Cosine Appprox with better formatting

--- SineApprox.m
+++ SineApprox.m
@@ -32,39 +32,53 @@ error = sin(pi*o/180) - ho
 % Funciones relacionadas a la interfaz de usuario.
 
 
-% Ejemplo de la tarea F=5/7;
+% Frecuencia del tono
 F=697;
-%Ejemplo de la tarea Fs=1;
+% Frecuencia de muestreo recomendada por el enunciado;
 Fs=8000;
-%Ejemplo de la tarea C=5;
-C=2;
+% En la practica, esto depende del tiempo que el usuario presione el boton;
+C=5;
 
 %Pasos intermedios auxiliares.
 
 
 %Calculamos el periodo de la señal analogica
 T=1/F;
-%Revisa el criterio de Nyquist
-%Si la frecuencia de muestreo es menor a la frecuencia de Nyquist
-if ( Fs < 2*F )
-%La frecuencia del alias es la frecuencia de muestreo menos la frecuencia real.
-  Fa=Fs-F
-% En caso contrario
-else
-% La señal se puede recuperar y no hay aliasing.
-  Fa=F
-endif 
+%Se asume que se cumple el criterio de Nyquist para todo
 
+% Se construye una variable analogica para comparar efectividad
 %Se construye el arreglo de la variable independiente analogica. Se elige arbitrariamente 100 puntos por periodo
 t=[0:0.0001*T:C*T];
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%       
+%
+%      Calculo de los valores de la señal discreta
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Construye el arreglo de la variable independiente discreta a partir del numero de ciclos y la tasa de muestreo
 n=[0:1/Fs:C*T];
-%Calcula los valores de la señal discreta
-%sinn=sin(2*pi*F*n);uahhñ
 
+% Coeficientes del polinomio
+c1 = 26-15*sqrt(3)
+c2 = 810000*(1657+930*sqrt(3))
+c3 = 324000*(19+15*sqrt(3))
+c4 = -1800*(1+15*sqrt(3))
+c5 = -360
+
+% Preprocesado
+
+% Conversion de variable discreta a grados entre 0 y 180 (region de convergencia para la aproximacion polinomial)
 o = mod(n*(2*F*180)+90,180);
+
+% Calculo del cuadrante mediante la diferencia entre el modulo a 180 y a 360 grados.
 o2 = mod(n*(2*F*180)+90,360);
 quadrant = 2*sign (o-o2)+1;
+
+% Calculo de la aproximacion cosenoidal en dos partes.
 part1=c2+c3*o+c4*(realpow(o,2))+c5*(realpow(o,3))+(realpow(o,4));
 sinn = quadrant.*( c1*o.*(180-o).*(part1) )/524880000000;
 
